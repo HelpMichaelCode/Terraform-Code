ef75a2afffe515be6164718bfd125c78812c4bc1
Michael Santos <michael.santos@openedgepay.com>
2021-09-22 22:49:27 +0100

    Created a static IP address for instance

--- LinkedIn_Learning_TF/main.tf
+++ LinkedIn_Learning_TF/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "0"
-  secret_key = "0"
+  access_key = "AKIAWVJQTWKM5WOED35O"
+  secret_key = "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"
 }
 
 resource "aws_s3_bucket" "my-bucket" {
@@ -37,6 +37,28 @@ resource "aws_security_group" "prod_web"{
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_instance" "prod_web" {
+  ami           = "ami-04fba448246555019"
+  instance_type = "t2.nano"
+
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id
+  ]
+
+   tags = {
+    "Terraform" : "true"
+  }
+}
+
+// static ip
+resource "aws_eip" "prod_web" {
+  instance = aws_instance.prod_web.id
 
   tags = {
     "Terraform" : "true"
